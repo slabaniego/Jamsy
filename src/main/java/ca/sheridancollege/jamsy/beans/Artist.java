23f8eb077053255639bc7f8f13d68af6fbc11667
simran242003 <simran33@sheridancollege.ca>
2025-04-06 20:39:30 -0400

    Updated Artist.java to the correct code after revert

--- src/main/java/ca/sheridancollege/jamsy/beans/Artist.java
+++ src/main/java/ca/sheridancollege/jamsy/beans/Artist.java
@@ -1,42 +1,18 @@
 package ca.sheridancollege.jamsy.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Artist {
-    private String name;
-    private String imageUrl;
-    private String previewUrl;
-    private List<String> genres;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getPreviewUrl() {
-        return previewUrl;
-    }
-
-    public void setPreviewUrl(String previewUrl) {
-        this.previewUrl = previewUrl;
-    }
-
-    public List<String> getGenres() {
-        return genres;
-    }
-
-    public void setGenres(List<String> genres) {
-        this.genres = genres;
-    }
+    private String name = "";
+    private String imageUrl = "";
+    private String previewUrl = "";
+    private List<String> genres = new ArrayList<>();
 }
